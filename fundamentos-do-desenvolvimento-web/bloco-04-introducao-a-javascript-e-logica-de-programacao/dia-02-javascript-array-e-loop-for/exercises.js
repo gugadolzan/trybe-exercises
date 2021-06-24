ba5d7210e01926114103f4a4f1d669f953788069
Gustavo Dolzan <gudolzan@gmail.com>
2021-06-24 18:25:30 -0300

    exercise 3

--- fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercises.js
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercises.js
@@ -9,10 +9,21 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // console.log(numbers);
 
 // ================> exercise 2 <================
+// let sum = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   sum = sum + numbers[index];
+// };
+
+// console.log(sum);
+
+// ================> exercise 3 <================
 let sum = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
   sum = sum + numbers[index];
 };
 
-console.log(sum);
+let average = sum / numbers.length;
+
+console.log(average);
