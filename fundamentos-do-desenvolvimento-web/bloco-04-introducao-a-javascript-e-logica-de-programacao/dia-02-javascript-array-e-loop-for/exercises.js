4edd253a751a2299e882a3fa2ced8d1ed05d85fd
Gustavo Dolzan <gudolzan@gmail.com>
2021-06-24 21:42:44 -0300

    bonus

--- fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercises.js
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercises.js
@@ -104,11 +104,11 @@
 
 
 // ================> exercise 8 <================
-let array = []
+// let array = []
 
-for (let index = 0; index < 25; index += 1) {
-  array.push(index + 1);
-}
+// for (let index = 0; index < 25; index += 1) {
+//   array.push(index + 1);
+// }
 
 // console.log(array);
 // ==============================================
@@ -117,6 +117,47 @@ for (let index = 0; index < 25; index += 1) {
 
 
 // ================> exercise 9 <================
-for (let index = 0; index < array.length; index += 1) {
-  console.log(array[index] / 2);
-}
+// for (let index = 0; index < array.length; index += 1) {
+//   console.log(array[index] / 2);
+// }
+// ==============================================
+
+
+
+
+// ================> bonus <================
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let index = 1; index < numbers.length; index += 1) {
+//   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//     if (numbers[index] < numbers[secondIndex]) {
+//       let position = numbers[index];
+//       numbers[index] = numbers[secondIndex];
+//       numbers[secondIndex] = position;
+//     }
+//   }
+// }
+
+// console.log('exercise 1: ' + numbers);
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//     if (numbers[index] > numbers[secondIndex]) {
+//       let position = numbers[index];
+//       numbers[index] = numbers[secondIndex];
+//       numbers[secondIndex] = position;
+//     }
+//   }
+// }
+
+// console.log('exercise 2: ' + numbers);
+
+// let numbersNew = [];
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (index + 1 < numbers.length) {
+//     numbersNew.push(numbers[index] * numbers[index + 1]);
+//   } else {
+//     numbersNew.push(numbers[index] * 2);
+//   };
+// }
+
+// console.log('exercise 3: ' + numbersNew);
