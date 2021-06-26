4d35248a0e088e05005dd770d645ae1859112a9d
Gustavo Dolzan <gudolzan@gmail.com>
2021-06-25 22:02:23 -0300

    exercise 5 bonus

--- fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercise_05_bonus.js
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercise_05_bonus.js
@@ -1,12 +1,12 @@
-let n = 5,
+let n = 7,
   output = '',
-  numberOfLines = Math.ceil(n / 2),
+  numberOfLines = (n + 1) / 2, // aka middle
   onLeft = numberOfLines,
   onRight = numberOfLines;
 
 for (let index = 1; index <= numberOfLines; index += 1) {
   for (let index2 = 1; index2 <= n; index2 += 1) {
-    if (index2 >= onLeft && index2 <= onRight) {
+    if (index2 === onLeft || index2 === onRight || index === numberOfLines) {
       output += '*';
     } else {
       output += ' ';
