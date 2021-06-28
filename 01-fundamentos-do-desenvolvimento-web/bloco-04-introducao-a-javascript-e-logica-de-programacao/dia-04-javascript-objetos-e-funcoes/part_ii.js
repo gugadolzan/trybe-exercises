bd402bfddd14c010f9bf2e63d79a803bca3d8ae6
Gustavo Dolzan <gudolzan@gmail.com>
2021-06-28 19:20:53 -0300

    Exercises 2 and 3, Part II

--- 01-fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/part_ii.js
+++ 01-fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/part_ii.js
@@ -1,14 +1,40 @@
-function verifyPalindrome(word) {
-  let palindrome = '';
-  for (let index = word.length - 1; index >= 0; index -= 1) {
-    palindrome += word[index];
-  }
-  if (word === palindrome) {
-    return true;
-  } else {
-    return false;
-  }
-};
+// function verifyPalindrome(word) {
+//   let palindrome = '';
+//   for (let index = word.length - 1; index >= 0; index -= 1) {
+//     palindrome += word[index];
+//   }
+//   if (word === palindrome) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 
-console.log(verifyPalindrome('arara'));
-console.log(verifyPalindrome('desenvolvimento'));
+// console.log(verifyPalindrome('arara'));
+// console.log(verifyPalindrome('desenvolvimento'));
+
+// console.log('****************');
+
+function indexOfHighest(numbers) {
+  let index = 0;
+  for (let i in numbers) {
+    if (numbers[i] > numbers[index]) {
+      index = i;
+    }
+  }
+  return index;
+};
+console.log(indexOfHighest([2, 3, 6, 7, 10, 1]));
+
+console.log('****************');
+
+function indexOfLowest(numbers) {
+  let index = 0;
+  for (let i in numbers) {
+    if (numbers[i] < numbers[index]) {
+      index = i;
+    }
+  }
+  return index;
+};
+console.log(indexOfLowest([2, 4, 6, 7, 10, 0, -3]));
