93a7a8bc5b50d94ddab8e393301b7f6e3b0bcca6
Gustavo Dolzan <gudolzan@gmail.com>
2021-06-28 19:25:31 -0300

    Exercise 4, Part II

--- 01-fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/part_ii.js
+++ 01-fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/part_ii.js
@@ -9,32 +9,44 @@
 //     return false;
 //   }
 // };
-
 // console.log(verifyPalindrome('arara'));
 // console.log(verifyPalindrome('desenvolvimento'));
 
 // console.log('****************');
 
-function indexOfHighest(numbers) {
-  let index = 0;
-  for (let i in numbers) {
-    if (numbers[i] > numbers[index]) {
-      index = i;
+// function indexOfHighest(numbers) {
+//   let index = 0;
+//   for (let i in numbers) {
+//     if (numbers[i] > numbers[index]) {
+//       index = i;
+//     }
+//   }
+//   return index;
+// };
+// console.log(indexOfHighest([2, 3, 6, 7, 10, 1]));
+
+// console.log('****************');
+
+// function indexOfLowest(numbers) {
+//   let index = 0;
+//   for (let i in numbers) {
+//     if (numbers[i] < numbers[index]) {
+//       index = i;
+//     }
+//   }
+//   return index;
+// };
+// console.log(indexOfLowest([2, 4, 6, 7, 10, 0, -3]));
+
+// console.log('****************');
+
+function longerWord(names) {
+  let longerWord = '';
+  for (let index in names) {
+    if (names[index].length > longerWord.length) {
+      longerWord = names[index];
     }
   }
-  return index;
+  return longerWord;
 };
-console.log(indexOfHighest([2, 3, 6, 7, 10, 1]));
-
-console.log('****************');
-
-function indexOfLowest(numbers) {
-  let index = 0;
-  for (let i in numbers) {
-    if (numbers[i] < numbers[index]) {
-      index = i;
-    }
-  }
-  return index;
-};
-console.log(indexOfLowest([2, 4, 6, 7, 10, 0, -3]));
+console.log(longerWord(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
