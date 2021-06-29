876cbca109942b3f327041244f828329e8205adc
Gustavo Dolzan <gudolzan@gmail.com>
2021-06-28 21:33:06 -0300

    Exercise 5, Part II

--- modulo-01-fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/part_ii_exercise_05.js
+++ modulo-01-fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/part_ii_exercise_05.js
@@ -1,2 +1,26 @@
-function mostRepeated(numbers) {
-};
+function mosRepeated(numbers) {
+  let num = {};
+
+  for (let index = 0; index < numbers.length; index += 1) { // faz a contagem de quantas vezes aparece e atribui à um objeto
+    let value = numbers[index];
+    if (num[value] === undefined) {
+      num[value] = 1;
+    } else {
+      num[value] = num[value] + 1;
+    }
+  };
+
+  let count = 0,
+    output = 0;
+
+  for (let key in num) { // verifica qual elemento do objeto possui a maior contagem
+    if (num[key] > count) {
+      count = num[key];
+      output = key;
+    }
+  };
+
+  return output;
+}
+
+console.log(mosRepeated([2, 3, 2, 5, 8, 2, 3]));
