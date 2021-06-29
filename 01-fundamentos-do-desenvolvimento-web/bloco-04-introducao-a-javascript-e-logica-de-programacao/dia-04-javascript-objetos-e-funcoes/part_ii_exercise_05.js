2e44a6db6d8616944876c8bcd2577c1103f5292f
Gustavo Dolzan <85767695+gugadolzan@users.noreply.github.com>
2021-06-28 22:06:02 -0300

    Revert "Exercises/4.4"

--- 01-fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/part_ii_exercise_05.js
+++
@@ -1,2 +0,0 @@
-function mostRepeated(numbers) {
-};
--- modulo-01-fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/part_ii_exercise_06.js
+++
@@ -1,8 +0,0 @@
-function sum(number) {
-  let sum = 0;
-  for (let count = 1; count <= number; count += 1) {
-    sum += count;
-  }
-  return sum;
-};
-console.log(sum(5));
