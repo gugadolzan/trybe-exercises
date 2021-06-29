e4b375980efb255eaff6cbf7e612a04118e0d4aa
Gustavo Dolzan <gudolzan@gmail.com>
2021-06-28 22:08:36 -0300

    Arranges files

---
+++ modulo-01-fundamentos-do-desenvolvimento-web/dia-04-javascript-objetos-e-funcoes/part_ii_exercise_01.js
@@ -0,0 +1,13 @@
+function verifyPalindrome(word) {
+  let palindrome = '';
+  for (let index = word.length - 1; index >= 0; index -= 1) {
+    palindrome += word[index];
+  }
+  if (word === palindrome) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(verifyPalindrome('arara'));
+console.log(verifyPalindrome('desenvolvimento'));
