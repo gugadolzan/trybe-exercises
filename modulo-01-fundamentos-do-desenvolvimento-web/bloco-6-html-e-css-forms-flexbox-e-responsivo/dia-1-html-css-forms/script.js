46a0c031bc96aa7885163a90120574f91008d361
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-14 20:34:38 -0300

    Exercise 2

--- modulo-01-fundamentos-do-desenvolvimento-web/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ modulo-01-fundamentos-do-desenvolvimento-web/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,11 +1,30 @@
-function fillsStates() {
+function fillStates() {
   const states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
   const selectState = document.getElementById('state');
-
   for (let index = 0; index < states.length; index += 1) {
     let state = document.createElement('option');
     state.innerText = states[index];
     selectState.appendChild(state);
   }
 }
-fillsStates();
+fillStates();
+
+function dateValidation() {
+  const date = document.getElementById('start-date').value;
+  let regexValidation = /^\d\d\/\d\d\/\d\d\d\d$/;
+  if (!regexValidation.test(date)) {
+    alert('Data de início: Formato inválido');
+  }
+  const dateArray = date.split('/');
+  if (dateArray[0] < 0 || dateArray[0] > 31) {
+    alert('Data de início: Dia inválido');
+  }
+  if (dateArray[1] < 0 || dateArray[1] > 12) {
+    alert('Data de início: Mês inválido');
+  }
+  if (dateArray[2] < 0) {
+    alert('Data de início: Ano inválido');
+  }
+}
+const submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', dateValidation);
