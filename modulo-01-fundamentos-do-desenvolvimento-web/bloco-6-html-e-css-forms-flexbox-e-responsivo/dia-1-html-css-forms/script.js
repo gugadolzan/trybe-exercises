4b8742cb476b45ba94016150974d51c64698e3e5
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-15 19:18:17 -0300

    Minor changes

--- modulo-01-fundamentos-do-desenvolvimento-web/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ modulo-01-fundamentos-do-desenvolvimento-web/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -4,6 +4,7 @@ function fillStates() {
   for (let index = 0; index < states.length; index += 1) {
     let state = document.createElement('option');
     state.innerText = states[index];
+    state.value = states[index];
     selectState.appendChild(state);
   }
 }
@@ -14,25 +15,41 @@ function dateValidation() {
   let regexValidation = /^\d\d\/\d\d\/\d\d\d\d$/;
   if (!regexValidation.test(date)) {
     alert('Data de início: Formato inválido');
+    return false;
   }
   const dateArray = date.split('/');
   if (dateArray[0] < 0 || dateArray[0] > 31) {
     alert('Data de início: Dia inválido');
+    return false;
   }
   if (dateArray[1] < 0 || dateArray[1] > 12) {
     alert('Data de início: Mês inválido');
+    return false;
   }
   if (dateArray[2] < 0) {
     alert('Data de início: Ano inválido');
+    return false;
   }
 }
+
 const submitButton = document.getElementById('submit');
+
 submitButton.addEventListener('click', dateValidation);
 
 const printForm = document.getElementById('print-form');
-submitButton.addEventListener('click', (event) => { // Not ok this function
+
+submitButton.addEventListener('click', (event) => {
   event.preventDefault();
-  for (let index = 0; index < document.getElementsByTagName('input').length - 1; index += 1) {
+  printForm.innerHTML = '';
+
+  for (let index = 0; index < 5; index += 1) {
+    printForm.innerHTML += document.getElementsByTagName('input')[index].value;
+    printForm.innerHTML += '<br>';
+  }
+  printForm.innerHTML += document.getElementById('resume').value;
+  printForm.innerHTML += '<br>';
+  // idk how to get state and house type values
+  for (let index = 7; index < 10; index += 1) {
     printForm.innerHTML += document.getElementsByTagName('input')[index].value;
     printForm.innerHTML += '<br>';
   }
