63811c5b7d4819dca48846ee8e6608dcc806087d
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-16 11:23:23 -0300

    New formatter

--- modulo-1-fundamentos-do-desenvolvimento-web/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ modulo-1-fundamentos-do-desenvolvimento-web/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,8 +1,36 @@
 function fillStates() {
-  const states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
-  const selectState = document.getElementById('state');
+  const states = [
+    "Acre",
+    "Alagoas",
+    "Amapá",
+    "Amazonas",
+    "Bahia",
+    "Ceará",
+    "Distrito Federal",
+    "Espírito Santo",
+    "Goiás",
+    "Maranhão",
+    "Mato Grosso",
+    "Mato Grosso do Sul",
+    "Minas Gerais",
+    "Pará",
+    "Paraíba",
+    "Paraná",
+    "Pernambuco",
+    "Piauí",
+    "Rio de Janeiro",
+    "Rio Grande do Norte",
+    "Rio Grande do Sul",
+    "Rondônia",
+    "Roraima",
+    "Santa Catarina",
+    "São Paulo",
+    "Sergipe",
+    "Tocantins",
+  ];
+  const selectState = document.getElementById("state");
   for (let index = 0; index < states.length; index += 1) {
-    let state = document.createElement('option');
+    let state = document.createElement("option");
     state.innerText = states[index];
     state.value = states[index];
     selectState.appendChild(state);
@@ -11,51 +39,51 @@ function fillStates() {
 fillStates();
 
 function dateValidation() {
-  const date = document.getElementById('start-date').value;
+  const date = document.getElementById("start-date").value;
   let regexValidation = /^\d\d\/\d\d\/\d\d\d\d$/;
   if (!regexValidation.test(date)) {
-    alert('Data de início: Formato inválido');
+    alert("Data de início: Formato inválido");
     return false;
   }
-  const dateArray = date.split('/');
+  const dateArray = date.split("/");
   if (dateArray[0] < 0 || dateArray[0] > 31) {
-    alert('Data de início: Dia inválido');
+    alert("Data de início: Dia inválido");
     return false;
   }
   if (dateArray[1] < 0 || dateArray[1] > 12) {
-    alert('Data de início: Mês inválido');
+    alert("Data de início: Mês inválido");
     return false;
   }
   if (dateArray[2] < 0) {
-    alert('Data de início: Ano inválido');
+    alert("Data de início: Ano inválido");
     return false;
   }
 }
 
-const submitButton = document.getElementById('submit');
+const submitButton = document.getElementById("submit");
 
-submitButton.addEventListener('click', dateValidation);
+submitButton.addEventListener("click", dateValidation);
 
-const printForm = document.getElementById('print-form');
+const printForm = document.getElementById("print-form");
 
-submitButton.addEventListener('click', (event) => {
+submitButton.addEventListener("click", (event) => {
   event.preventDefault();
-  printForm.innerHTML = '';
+  printForm.innerHTML = "";
 
   for (let index = 0; index < 5; index += 1) {
-    printForm.innerHTML += document.getElementsByTagName('input')[index].value;
-    printForm.innerHTML += '<br>';
+    printForm.innerHTML += document.getElementsByTagName("input")[index].value;
+    printForm.innerHTML += "<br>";
   }
-  printForm.innerHTML += document.getElementById('resume').value;
-  printForm.innerHTML += '<br>';
+  printForm.innerHTML += document.getElementById("resume").value;
+  printForm.innerHTML += "<br>";
   // idk how to get state and house type values
   for (let index = 7; index < 10; index += 1) {
-    printForm.innerHTML += document.getElementsByTagName('input')[index].value;
-    printForm.innerHTML += '<br>';
+    printForm.innerHTML += document.getElementsByTagName("input")[index].value;
+    printForm.innerHTML += "<br>";
   }
 });
 
-const resetButton = document.getElementById('reset');
-resetButton.addEventListener('click', () => {
-  printForm.innerHTML = '';
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", () => {
+  printForm.innerHTML = "";
 });
