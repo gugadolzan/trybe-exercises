4735c9771620df4e8863c9dd79f62cc11325b63c
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-29 01:45:47 -0300

    Bonus - Part I - Exercise 1

--- modulo-1-fundamentos-do-desenvolvimento-web/bloco-08-higher-order-functions-do-javascript-es6/01-javascript-es6-introducao-a-higher-order-functions/exercise-2.js
+++ modulo-1-fundamentos-do-desenvolvimento-web/bloco-08-higher-order-functions-do-javascript-es6/01-javascript-es6-introducao-a-higher-order-functions/exercise-2.js
@@ -1,5 +1,6 @@
 const didIWin = (myNumber, raffleNumber) => myNumber === raffleNumber;
 
-const raffle = (myNumber, callback) => (callback(myNumber, Math.ceil(Math.random() * 5))) ? 'Parabéns você ganhou' : 'Tente novamente';
+const raffle = (myNumber, callback) => (callback(myNumber, Math.floor(Math.random() * 5 + 1))) ? 'Parabéns você ganhou' : 'Tente novamente';
 
 console.log(raffle(2, didIWin));
+ 
