b06c8fb2a8ada235655b7377a735e5ca18dab7a2
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-31 01:25:53 -0300

    Bonus - Part II - Exercise 1

--- modulo-1-fundamentos-do-desenvolvimento-web/bloco-08-higher-order-functions-do-javascript-es6/01-javascript-es6-introducao-a-higher-order-functions/game-actions-simulator.js
+++ modulo-1-fundamentos-do-desenvolvimento-web/bloco-08-higher-order-functions-do-javascript-es6/01-javascript-es6-introducao-a-higher-order-functions/game-actions-simulator.js
@@ -21,8 +21,16 @@ const dragon = {
 const battleMembers = { mage, warrior, dragon };
 
 // To generate a random number between two numbers: Math.floor(Math.random() * (maxNumber + 1 (to include maxNumber) - minNumber) + minNumber);
-const dragonDamage = (dragon) => Math.floor(Math.random() * (dragon.strength + 1 - 15) + 15);
+const dragonAttack = (dragon) => Math.floor(Math.random() * (dragon.strength + 1 - 15) + 15);
 
-const warriorDamage = (warrior) => Math.floor(Math.random() * (warrior.strength * warrior.weaponDmg + 1 - warrior.strength) + warrior.strength);
+const warriorAttack = (warrior) => Math.floor(Math.random() * (warrior.strength * warrior.weaponDmg + 1 - warrior.strength) + warrior.strength);
 
-const mageDamage = (mage) => (mage.mana < 15) ? { damage: 'Não possui mana suficiente', manaSpent: 0 } : { damage: Math.floor(Math.random() * (mage.intelligence * 2 + 1 - mage.intelligence) + mage.intelligence), manaSpent: 15 };
+const mageAttack = (mage) => (mage.mana < 15) ? { damage: 'Não possui mana suficiente', manaSpent: 0 } : { damage: Math.floor(Math.random() * (mage.intelligence * 2 + 1 - mage.intelligence) + mage.intelligence), manaSpent: 15 };
+
+const gameActions = {
+  warriorTurn: (warriorAttack) => {
+    const warriorDamage = warriorAttack(warrior);
+    dragon.healthPoints -= warriorDamage;
+    warrior.damage = warriorDamage;
+  }
+};
