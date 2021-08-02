18adbc7237316ec09f44ae7ed120909d62d41e1c
Gustavo Dolzan <gudolzan@gmail.com>
2021-08-02 18:28:17 -0300

    Exercise 1

---
+++ modulo-1-fundamentos-do-desenvolvimento-web/bloco-08-higher-order-functions-do-javascript-es6/04-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  return arrays.reduce((acc, element) => {
+    element.forEach(value => acc.push(value));
+    return acc;
+  }, []);
+}
+
+// function flatten() {
+//   return arrays.reduce((acc, curr) => acc.concat(curr), []);
+// }
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
