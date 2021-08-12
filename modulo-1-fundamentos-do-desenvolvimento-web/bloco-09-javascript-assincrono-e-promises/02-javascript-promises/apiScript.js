9bd39ed3e2fe9e283178ca075626289c08aa3352
Gustavo Dolzan <gudolzan@gmail.com>
2021-08-11 21:07:18 -0300

    Adds comments and prints response return from fetch function

--- modulo-1-fundamentos-do-desenvolvimento-web/bloco-09-javascript-assincrono-e-promises/02-javascript-promises/apiScript.js
+++ modulo-1-fundamentos-do-desenvolvimento-web/bloco-09-javascript-assincrono-e-promises/02-javascript-promises/apiScript.js
@@ -1,12 +1,13 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  const myObject = {
+  const myObject = { // myObject defines request type (method: 'GET') and response format (headers: { 'Accept': 'application/json' })
     method: 'GET',
     headers: { Accept: 'application/json' },
   };
 
-  fetch(API_URL, myObject);
+  fetch(API_URL, myObject) // fetch function returns response object
+    .then(response => console.log(response));
 };
 
 window.onload = () => fetchJoke();
