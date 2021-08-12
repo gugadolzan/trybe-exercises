f9a8152a223bc8d27e42c31db5e849e91c72d319
Gustavo Dolzan <gudolzan@gmail.com>
2021-08-11 21:42:56 -0300

    Finalizes exercise 1 by adding the joke into HTML

--- modulo-1-fundamentos-do-desenvolvimento-web/bloco-09-javascript-assincrono-e-promises/02-javascript-promises/apiScript.js
+++ modulo-1-fundamentos-do-desenvolvimento-web/bloco-09-javascript-assincrono-e-promises/02-javascript-promises/apiScript.js
@@ -1,14 +1,18 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  const myObject = { // myObject defines request type (method: 'GET') and response format (headers: { 'Accept': 'application/json' })
+  const myObject = {
+    // myObject defines request type (method: 'GET') and response format (headers: { 'Accept': 'application/json' })
     method: 'GET',
     headers: { Accept: 'application/json' },
   };
 
   fetch(API_URL, myObject) // fetch function returns response object
-    .then(response => response.json()) // .json() converts the contents of the body into a JSON return
-    .then(data => console.log(data));
+    .then((response) => response.json()) // .json() converts the contents of the body into a JSON return
+    // .then(data => console.log(data));
+    .then(({ joke }) => {
+      document.getElementById('jokeContainer').innerHTML = joke;
+    });
 };
 
 window.onload = () => fetchJoke();
