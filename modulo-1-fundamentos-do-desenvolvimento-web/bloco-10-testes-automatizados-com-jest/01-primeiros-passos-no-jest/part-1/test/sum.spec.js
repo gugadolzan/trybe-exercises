edcf61bcfd3b976c66639cb27d62416296c58115
Gustavo Dolzan <gudolzan@gmail.com>
2021-08-26 01:06:15 -0300

    Part I - Exercise 2

--- modulo-1-fundamentos-do-desenvolvimento-web/bloco-10-testes-automatizados-com-jest/01-primeiros-passos-no-jest/part-1/test/sum.spec.js
+++ modulo-1-fundamentos-do-desenvolvimento-web/bloco-10-testes-automatizados-com-jest/01-primeiros-passos-no-jest/part-1/test/sum.spec.js
@@ -1,21 +1,21 @@
 const sum = require('../src/sum');
 
-describe('sum(a, b) returns the sum of parameter a and b', () => {
-  it('should return 9 sum(4,5)', () => {
+describe('A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
+  it('Teste se o retorno de sum(4, 5) é 9', () => {
     expect(sum(4, 5)).toBe(9);
   });
 
-  it('should return 0 when sum(0,0)', () => {
+  it('Teste se o retorno de sum(0, 0) é 0', () => {
     expect(sum(0, 0)).toBe(0);
   });
 
-  it("should throw an error when the parameters are 4 and '5' (string 5)", () => {
+  it('Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', () => {
     expect(() => {
       sum(4, '5');
     }).toThrow();
   });
 
-  it("should error message be 'parameters must be numbers' when calling sum(4, '5')", () => {
+  it('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
     expect(() => {
       sum('4', 5);
     }).toThrowError(new Error('parameters must be numbers'));
