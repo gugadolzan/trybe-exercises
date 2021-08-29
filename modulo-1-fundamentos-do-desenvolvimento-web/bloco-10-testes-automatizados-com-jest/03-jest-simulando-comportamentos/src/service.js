4b7b7e69f37638d231a435de1461fa00f399db23
Gustavo Dolzan <gudolzan@gmail.com>
2021-08-29 19:29:20 -0300

    Creates exercise 04 functions

--- modulo-1-fundamentos-do-desenvolvimento-web/bloco-10-testes-automatizados-com-jest/03-jest-simulando-comportamentos/src/service.js
+++ modulo-1-fundamentos-do-desenvolvimento-web/bloco-10-testes-automatizados-com-jest/03-jest-simulando-comportamentos/src/service.js
@@ -1,3 +1,11 @@
+// Exercises 01-03
 const randomNumber = () => Math.floor(Math.random() * 101);
 
-module.exports = { randomNumber };
+// Exercises 04-05
+const toUpperCase = (str) => str.toUpperCase();
+
+const firstChar = (str) => str[0];
+
+const concatStrings = (str1, str2) => str1.concat(str2);
+
+module.exports = { randomNumber, toUpperCase, firstChar, concatStrings };
