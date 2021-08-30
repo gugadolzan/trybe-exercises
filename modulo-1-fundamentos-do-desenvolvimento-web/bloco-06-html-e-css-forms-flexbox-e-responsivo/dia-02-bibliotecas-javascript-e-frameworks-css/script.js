a1402612d1f390f15131d83c350d8ff1e150954f
Gustavo Dolzan <gudolzan@gmail.com>
2021-08-29 23:02:41 -0300

    Installs just validate and formats files

--- modulo-1-fundamentos-do-desenvolvimento-web/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
+++ modulo-1-fundamentos-do-desenvolvimento-web/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,5 +1,33 @@
 function fillStates() {
-  const states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+  const states = [
+    'Acre',
+    'Alagoas',
+    'Amapá',
+    'Amazonas',
+    'Bahia',
+    'Ceará',
+    'Distrito Federal',
+    'Espírito Santo',
+    'Goiás',
+    'Maranhão',
+    'Mato Grosso',
+    'Mato Grosso do Sul',
+    'Minas Gerais',
+    'Pará',
+    'Paraíba',
+    'Paraná',
+    'Pernambuco',
+    'Piauí',
+    'Rio de Janeiro',
+    'Rio Grande do Norte',
+    'Rio Grande do Sul',
+    'Rondônia',
+    'Roraima',
+    'Santa Catarina',
+    'São Paulo',
+    'Sergipe',
+    'Tocantins',
+  ];
   const selectState = document.getElementById('state');
   for (let index = 0; index < states.length; index += 1) {
     let state = document.createElement('option');
@@ -10,31 +38,7 @@ function fillStates() {
 }
 fillStates();
 
-// function dateValidation() {
-//   const date = document.getElementById('start-date').value;
-//   let regexValidation = /^\d\d\/\d\d\/\d\d\d\d$/;
-//   if (!regexValidation.test(date)) {
-//     alert('Data de início: Formato inválido');
-//     return false;
-//   }
-//   const dateArray = date.split('/');
-//   if (dateArray[0] < 0 || dateArray[0] > 31) {
-//     alert('Data de início: Dia inválido');
-//     return false;
-//   }
-//   if (dateArray[1] < 0 || dateArray[1] > 12) {
-//     alert('Data de início: Mês inválido');
-//     return false;
-//   }
-//   if (dateArray[2] < 0) {
-//     alert('Data de início: Ano inválido');
-//     return false;
-//   }
-// }
-
 const submitButton = document.getElementById('submit');
-
-// submitButton.addEventListener('click', dateValidation);
 
 const printForm = document.getElementById('print-form');
 
@@ -48,7 +52,7 @@ submitButton.addEventListener('click', (event) => {
   }
   printForm.innerHTML += document.getElementById('resume').value;
   printForm.innerHTML += '<br>';
-  // idk how to get state and house type values
+
   for (let index = 7; index < 10; index += 1) {
     printForm.innerHTML += document.getElementsByTagName('input')[index].value;
     printForm.innerHTML += '<br>';
@@ -61,5 +65,5 @@ resetButton.addEventListener('click', () => {
 });
 
 var picker = new Pikaday({
-  field: document.getElementById('start-date')
+  field: document.getElementById('start-date'),
 });
