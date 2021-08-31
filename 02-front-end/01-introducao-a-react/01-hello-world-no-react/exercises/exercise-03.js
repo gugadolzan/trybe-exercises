2bba4ff3876a15d7d4192983b5ab24cde03799f1
Gustavo Dolzan <gudolzan@gmail.com>
2021-08-31 11:42:10 -0300

    Exercise 03 - Create a Complex JSX Element

---
+++ 02-front-end/01-introducao-a-react/01-hello-world-no-react/exercises/exercise-03.js
@@ -0,0 +1,17 @@
+// Create a Simple JSX Element
+
+// const JSX = <h1>Hello JSX!</h1>;
+
+// Create a Complex JSX Element
+
+// const JSX = (
+//   <div>
+//     <h1>h1</h1>
+//     <p>p</p>
+//     <ul>
+//       <li>li 1</li>
+//       <li>li 1</li>
+//       <li>li 1</li>
+//     </ul>
+//   </div>
+// );
