89f83cca2f7480c920ae9f67b587f6ea5da194e7
Gustavo Dolzan <gudolzan@gmail.com>
2021-08-31 12:03:06 -0300

    Exercise 03 - Learn About Self-Closing JSX Tags

--- 02-front-end/01-introducao-a-react/01-hello-world-no-react/exercises/exercise-03.js
+++ 02-front-end/01-introducao-a-react/01-hello-world-no-react/exercises/exercise-03.js
@@ -44,3 +44,13 @@
 //     <h1>Add a class to this div</h1>
 //   </div>
 // );
+
+// Learn About Self-Closing JSX Tags
+
+// const JSX = (
+//   <div>
+//     <h2>Welcome to React!</h2> <br />
+//     <p>Be sure to close all tags!</p>
+//     <hr />
+//   </div>
+// );
