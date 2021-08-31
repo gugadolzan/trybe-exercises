693a845ddc7ee83fbafe227a2c011d13313f8b07
Gustavo Dolzan <gudolzan@gmail.com>
2021-08-31 12:13:19 -0300

    Exercise 03 - Create a React Component

--- 02-front-end/01-introducao-a-react/01-hello-world-no-react/exercises/exercise-03.js
+++ 02-front-end/01-introducao-a-react/01-hello-world-no-react/exercises/exercise-03.js
@@ -64,3 +64,18 @@
 //     </div>
 //   );
 // };
+
+// Create a React Component
+
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello React!</h1>
+//       </div>
+//     );
+//   }
+// }
