f8e01498982f312c04cb9534b9066caea21abacc
Gustavo Dolzan <gudolzan@gmail.com>
2021-08-31 12:19:54 -0300

    Exercise 03 - Create a Component with Composition

--- 02-front-end/01-introducao-a-react/01-hello-world-no-react/exercises/exercise-03.js
+++ 02-front-end/01-introducao-a-react/01-hello-world-no-react/exercises/exercise-03.js
@@ -79,3 +79,27 @@
 //     );
 //   }
 // }
+
+// Create a Component with Composition
+
+// const ChildComponent = () => {
+//   return (
+//     <div>
+//       <p>I am the child</p>
+//     </div>
+//   );
+// };
+
+// class ParentComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>I am the parent</h1>
+//         <ChildComponent />
+//       </div>
+//     );
+//   }
+// }
