f084e8d3a5a3cded2c7f45903a8d0ffe3305bbe0
Gustavo Dolzan <gudolzan@gmail.com>
2021-09-02 20:47:30 -0300

    Render single Pokemon

--- 02-front-end/01-introducao-a-react/02-componentes-react/my-pokedex/src/App.js
+++ 02-front-end/01-introducao-a-react/02-componentes-react/my-pokedex/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Pokemon from './components/Pokemon';
+import pokemons from './data';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <Pokemon />;
+    return <Pokemon pokemon={pokemons[0]} />;
   }
 }
 
--- 02-front-end/01-introducao-a-react/02-componentes-react/my-pokedex/src/components/Pokemon.jsx
+++ 02-front-end/01-introducao-a-react/02-componentes-react/my-pokedex/src/components/Pokemon.jsx
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 
 class Pokemon extends Component {
   render() {
-    return <p>Pokémon</p>;
+    console.log(this.props);
+
+    const { pokemon } = this.props;
+    const { id, name, type, averageWeight, image, moreInfo } = pokemon;
+
+    return (
+      <>
+        <p>{id}</p>
+        <p>{name}</p>
+        <p>{type}</p>
+        <p>{averageWeight.value + averageWeight.measurementUnit}</p>
+        <p>{image}</p>
+        <p>{moreInfo}</p>
+      </>
+    );
   }
 }
 
