50cc932a06b82422c8fdd2a883f4e32ae989b645
Gustavo Dolzan <gudolzan@gmail.com>
2021-09-08 02:18:08 -0300

    2 - Create new fieldset

--- 02-front-end/12-componentes-com-estado-eventos-e-formularios-com-react/02-formularios-no-react/my-form-2.0/src/App.js
+++ 02-front-end/12-componentes-com-estado-eventos-e-formularios-com-react/02-formularios-no-react/my-form-2.0/src/App.js
@@ -1,95 +1,95 @@
 import React, { Component } from 'react';
+import states from './data';
 import './App.css';
 
 class App extends Component {
   render() {
-    const states = [
-      'AC',
-      'AL',
-      'AM',
-      'AP',
-      'BA',
-      'CE',
-      'DF',
-      'ES',
-      'GO',
-      'MA',
-      'MT',
-      'MS',
-      'MG',
-      'PA',
-      'PB',
-      'PR',
-      'PE',
-      'PI',
-      'RJ',
-      'RN',
-      'RO',
-      'RS',
-      'RR',
-      'SC',
-      'SE',
-      'SP',
-      'TO',
-    ];
-
     return (
       <form>
-        <label htmlFor="name">
-          Nome:
-          <input id="name" name="name" type="text" />
-        </label>
+        <fieldset>
+          <legend>Dados pessoais</legend>
 
-        <label htmlFor="email">
-          Email:
-          <input id="email" name="email" type="text" />
-        </label>
+          <label htmlFor="name">
+            Nome:
+            <input id="name" name="name" type="text" />
+          </label>
 
-        <label htmlFor="cpf">
-          CPF:
-          <input id="cpf" name="cpf" type="text" />
-        </label>
+          <label htmlFor="email">
+            Email:
+            <input id="email" name="email" type="text" />
+          </label>
 
-        <label htmlFor="address">
-          Endereço:
-          <input id="address" name="address" type="text" />
-        </label>
+          <label htmlFor="cpf">
+            CPF:
+            <input id="cpf" name="cpf" type="text" />
+          </label>
 
-        <label htmlFor="city">
-          Cidade:
-          <input id="city" name="city" type="text" />
-        </label>
+          <label htmlFor="address">
+            Endereço:
+            <input id="address" name="address" type="text" />
+          </label>
 
-        <label htmlFor="state">
-          Estado:
-          <select id="state" name="state" defaultValue="">
-            <option value="" disabled>
-              Selecione
-            </option>
-            {states.map((state) => (
-              <option key={state} value={state}>
-                {state}
+          <label htmlFor="city">
+            Cidade:
+            <input id="city" name="city" type="text" />
+          </label>
+
+          <label htmlFor="state">
+            Estado:
+            <select id="state" name="state">
+              <option disabled value="" selected>
+                Selecione
               </option>
-            ))}
-          </select>
-        </label>
 
-        <label htmlFor="house">
-          Tipo de moradia:
-          <label>
-            <input id="house" type="radio" name="addressType" value="house" />{' '}
-            Casa
+              {states.map((state) => (
+                <option key={state} value={state}>
+                  {state}
+                </option>
+              ))}
+            </select>
           </label>
-          <label htmlFor="apartment">
-            <input
-              id="apartment"
-              type="radio"
-              name="addressType"
-              value="apartment"
-            />{' '}
-            Apartamento
+
+          <label htmlFor="house">
+            Tipo de moradia:
+            <label>
+              <input id="house" name="addressType" type="radio" value="house" />{' '}
+              Casa
+            </label>
+            <label htmlFor="apartment">
+              <input
+                id="apartment"
+                name="addressType"
+                type="radio"
+                value="apartment"
+              />{' '}
+              Apartamento
+            </label>
           </label>
-        </label>
+        </fieldset>
+
+        <fieldset>
+          <legend>Dados do seu último emprego</legend>
+
+          <label htmlFor="resume">
+            Resumo do currículo
+            <textarea id="resume" maxLength="1000" name="resume" required />
+          </label>
+
+          <label htmlFor="role">
+            Cargo
+            <input maxLength="40" name="role" required type="text" />
+          </label>
+
+          <label htmlFor="roleDescription">
+            Descrição do cargo:
+            <textarea
+              id="roleDescription"
+              maxLength="500"
+              name="roleDescription"
+              required
+            />
+          </label>
+        </fieldset>
       </form>
     );
   }
