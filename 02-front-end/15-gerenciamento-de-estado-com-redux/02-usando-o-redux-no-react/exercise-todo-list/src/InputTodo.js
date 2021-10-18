75bfd1b0bb37e584e9b83541ff9c5300e15b24c4
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-18 00:24:50 -0300

    Refactor InputTodo component

--- 02-front-end/15-gerenciamento-de-estado-com-redux/02-usando-o-redux-no-react/exercise-todo-list/src/InputTodo.js
+++ 02-front-end/15-gerenciamento-de-estado-com-redux/02-usando-o-redux-no-react/exercise-todo-list/src/InputTodo.js
@@ -1,43 +1,29 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addTodo } from './actions';
 
-class InputTodo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      textTodo: '',
-    };
-
-    this.changeTextTodo = this.changeTextTodo.bind(this);
-  }
-
-  changeTextTodo(value) {
-    this.setState({ textTodo: value })
-  }
-
-  addItem(value, callback) {
-    this.setState({ textTodo: '' })
-    callback(value)
-  }
-
-  render() {
-    const { addTodo } = this.props;
-    const { textTodo } = this.state;
-    return (
-      <div className="InputTodo">
-        <label htmlFor="inputTodo">Tarefa:</label>
-        <input
-          id="inputTodo"
-          type="text"
-          value={textTodo}
-          onChange={(e) => this.changeTextTodo(e.target.value)}
-        />
-        <input id="btnAdd" type="button" value="Adicionar" onClick={() => this.addItem(textTodo,addTodo)} />
-      </div>
-    );
-  }
+function InputTodo(props) {
+  const { addTodo } = props;
+  let inputElement;
+  return (
+    <form
+      className="InputTodo"
+      onSubmit={(e) => {
+        e.preventDefault();
+        addTodo(inputElement.value);
+        inputElement.value = '';
+      }}
+    >
+      <label htmlFor="inputTodo">Tarefa:</label>
+      <input
+        ref={(input) => {
+          inputElement = input;
+        }}
+      />
+      <button type="submit">Adicionar</button>
+    </form>
+  );
 }
 
 const mapDispatchToProps = (dispatch) => ({
@@ -48,4 +34,4 @@ export default connect(null, mapDispatchToProps)(InputTodo);
 
 InputTodo.propTypes = {
   addTodo: PropTypes.func.isRequired,
-}
+};
