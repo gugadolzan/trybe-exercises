607fe7c66e8f0a2e35ca1238b524aa3d37f5c832
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-19 22:52:36 -0300

    Implement Register.js

--- 02-front-end/15-gerenciamento-de-estado-com-redux/03-usando-o-redux-no-react-pratica/exercise-bonus/src/pages/Register.js
+++ 02-front-end/15-gerenciamento-de-estado-com-redux/03-usando-o-redux-no-react-pratica/exercise-bonus/src/pages/Register.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Register() {
-  return (
-    <div>
-      <h1>Register</h1>
-    </div>
-  );
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      age: '',
+      email: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { name, age, email } = this.state;
+    return (
+      <div>
+        <label htmlFor="name">Nome</label>
+        <input
+          name="name"
+          onChange={this.handleChange}
+          type="text"
+          value={name}
+        />
+        <br />
+        <label htmlFor="age">Idade</label>
+        <input
+          name="age"
+          onChange={this.handleChange}
+          type="number"
+          value={age}
+        />
+        <br />
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          onChange={this.handleChange}
+          type="text"
+          value={email}
+        />
+        <br />
+        <button type="button">Cadastrar</button>
+        <br />
+        <Link to="/clients">Clientes Cadastrados</Link>
+      </div>
+    );
+  }
 }
+
+export default Register;
