38cba31768e12cf9305f28b27c93e2d8977574c0
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-19 21:40:59 -0300

    Implement Login.js

--- 02-front-end/15-gerenciamento-de-estado-com-redux/03-usando-o-redux-no-react-pratica/exercise-bonus/src/pages/Login.js
+++ 02-front-end/15-gerenciamento-de-estado-com-redux/03-usando-o-redux-no-react-pratica/exercise-bonus/src/pages/Login.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  );
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div>
+        <div>
+          <input
+            name="email"
+            onChange={this.handleChange}
+            placeholder="Email"
+            type="text"
+            value={email}
+          />
+          <input
+            name="password"
+            onChange={this.handleChange}
+            placeholder="Senha"
+            type="password"
+            value={password}
+          />
+        </div>
+        <Link to="/clients">Fazer login</Link>
+      </div>
+    );
+  }
 }
+
+export default Login;
