d393df8bc42ac2fea48789903dc0e348b16e1c26
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-19 21:33:33 -0300

    Implement Home.js

--- 02-front-end/15-gerenciamento-de-estado-com-redux/03-usando-o-redux-no-react-pratica/exercise-bonus/src/pages/Home.js
+++ 02-front-end/15-gerenciamento-de-estado-com-redux/03-usando-o-redux-no-react-pratica/exercise-bonus/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <h1>Home</h1>
-    </div>
+      <Link to="/Login">Faça seu Login</Link>
+    </>
   );
 }
