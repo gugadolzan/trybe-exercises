5d548f8924a0980b1be109263e249a3d107f6b0d
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-19 22:05:36 -0300

    Create registerReducer

--- 02-front-end/15-gerenciamento-de-estado-com-redux/03-usando-o-redux-no-react-pratica/exercise-bonus/src/pages/Clients.js
+++ 02-front-end/15-gerenciamento-de-estado-com-redux/03-usando-o-redux-no-react-pratica/exercise-bonus/src/pages/Clients.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-function Clients({ userLogin }) {
+function Clients({ userLogin, registers }) {
+  const registerLink = <Link to="/register">Ir à pagina de cadastro</Link>;
+
   if (!userLogin.email || !userLogin.password)
     return <div>Login não efetuado</div>;
 
-  return (
-    <div>
-      <Link to="/register">Ir à pagina de cadastro</Link>
-    </div>
-  );
+  if (!registers.length)
+    return (
+      <div>
+        <div>Nenhum cliente cadastrado</div>
+        {registerLink}
+      </div>
+    );
+
+  return registerLink;
 }
 
 const mapStateToProps = (state) => ({
   userLogin: state.loginReducer,
+  registers: state.registerReducer,
 });
 
 export default connect(mapStateToProps)(Clients);
---
+++ 02-front-end/15-gerenciamento-de-estado-com-redux/03-usando-o-redux-no-react-pratica/exercise-bonus/src/redux/reducers/register.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = [];
+
+function registerReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default registerReducer;
