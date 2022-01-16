97953dee9d42d3e8a121fa527bb577167fc4b6b2
Gustavo Dolzan <gudolzan@gmail.com>
2022-01-16 15:21:51 -0300

    5 - Return situation

--- 03-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/01-nodejs-um-motor-javascript/imc.js
+++ 03-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/01-nodejs-um-motor-javascript/imc.js
@@ -1,8 +1,31 @@
-const readline = require("readline-sync");
+const readline = require('readline-sync');
 
+/**
+ * Retorna a situação do IMC
+ * @param {number} imc
+ * @returns {string}
+ */
+const situacaoIMC = (imc) => {
+  if (imc < 18.5) return 'Abaixo do peso (magreza)';
+
+  if (imc >= 18.5 && imc < 25) return 'Peso normal';
+
+  if (imc >= 25 && imc < 30) return 'Acima do peso (sobrepeso)';
+
+  if (imc >= 30 && imc < 35) return 'Obesidade grau I';
+
+  if (imc >= 35 && imc < 40) return 'Obesidade grau II';
+
+  return 'Obesidade graus III e IV';
+};
+
+/**
+ * Calcula o IMC e retorna a situação
+ * @returns {string}
+ */
 const calculaIMC = () => {
-  const peso = readline.questionFloat("Qual seu peso? ");
-  const altura = readline.questionFloat("Qual sua altura? ");
+  const peso = readline.questionFloat('Qual seu peso? ');
+  const altura = readline.questionFloat('Qual sua altura? ');
 
   console.log(`Peso: ${peso} Kg`);
   console.log(`Altura: ${altura} m`);
@@ -10,6 +33,7 @@ const calculaIMC = () => {
   const imc = (peso / (altura * altura)).toFixed(2);
 
   console.log(`IMC: ${imc}`);
+  console.log(`Situação: ${situacaoIMC(Number(imc))}`);
 
   return;
 };
