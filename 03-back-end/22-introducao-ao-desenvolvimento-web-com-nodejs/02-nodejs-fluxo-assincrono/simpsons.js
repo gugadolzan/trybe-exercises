b20303451aeb58ef902cdd55b37e4f15ec3a454d
Gustavo Dolzan <gudolzan@gmail.com>
2022-01-29 17:58:16 -0300

    Exercise 04.2 - Create getCharacterById function

--- 03-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/02-nodejs-fluxo-assincrono/simpsons.js
+++ 03-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/02-nodejs-fluxo-assincrono/simpsons.js
@@ -15,3 +15,40 @@ const logCharacters = async () => {
     console.error(err);
   }
 };
+
+/**
+ * @description Recebe o id de um personagem como parâmetro e retorna uma Promise que é resolvida com os dados do personagem que possui o id informado
+ * @param {number} id
+ * @returns {Promise}
+ */
+const getCharacterById = async (id) => {
+  try {
+    const fileContent = await fs.readFile(fileName);
+    const characters = JSON.parse(fileContent);
+    const character = characters.find(
+      ({ id: characterId }) => characterId === String(id)
+    );
+
+    /* A palavra-chave `throw` dispara um erro que deve ser tratado por quem chamou nossa função.
+     * Em funções `async`, utilizar `throw` faz com que a Promise seja rejeitada,
+     * tendo como motivo o que passarmos para o `throw`.
+     * Ou seja, a linha abaixo rejeita a Promise da nossa função com o motivo 'id não encontrado'
+     */
+    if (!character) throw new Error('id não encontrado');
+
+    /* Da mesma forma que `throw` aciona o fluxo de erro e rejeita a Promise,
+     * `return` aciona o fluxo de sucesso e resolve a Promise.
+     * Sendo assim, a linha abaixo é equivalente a chamar `resolve(chosenSimpson)`
+     * dentro do executor de uma Promise.
+     */
+    return character;
+
+    /* Alternative
+    if (!character) return Promise.reject('id não encontrado');
+
+    return Promise.resolve(character);
+    */
+  } catch (err) {
+    console.error(err);
+  }
+};
