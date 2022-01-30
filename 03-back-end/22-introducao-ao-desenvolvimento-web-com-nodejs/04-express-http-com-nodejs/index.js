3e4487e0dcbc35d8079323ad7aff6ead772ccf58
Gustavo Dolzan <gudolzan@gmail.com>
2022-01-30 03:16:14 -0300

    Create POST /signup endpoint to generate random token

--- 03-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/04-express-http-com-nodejs/index.js
+++ 03-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/04-express-http-com-nodejs/index.js
@@ -1,26 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const authMiddleware = require('./authMiddleware');
+const generateToken = require('./generateToken');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(authMiddleware);
 
 const PORT = 3000;
+const HTTP_STATUS_OK = 200;
+const HTTP_STATUS_UNAUTHORIZED = 401;
 
-app.get('/ping', (_req, res) => res.json({ message: 'pong' }));
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
 
-app.post('/hello', ({ body: { name } }, res) =>
+  if (!email || !password || !firstName || !phone)
+    return res
+      .status(HTTP_STATUS_UNAUTHORIZED)
+      .json({ message: 'Missing fields' });
+
+  const token = generateToken();
+
+  res.status(HTTP_STATUS_OK).json({ token });
+});
+
+app.get('/ping', authMiddleware, (_req, res) => res.json({ message: 'pong' }));
+
+app.post('/hello', authMiddleware, ({ body: { name } }, res) =>
   res.json({ message: `Hello, ${name}!` })
 );
 
-app.post('/greetings', ({ body: { name, age } }, res) =>
+app.post('/greetings', authMiddleware, ({ body: { name, age } }, res) =>
   age > 17
-    ? res.status(200).json({ message: `Hello, ${name}!` })
-    : res.status(401).json({ message: 'Unauthorized' })
+    ? res.status(HTTP_STATUS_OK).json({ message: `Hello, ${name}!` })
+    : res.status(HTTP_STATUS_UNAUTHORIZED).json({ message: 'Unauthorized' })
 );
 
-app.put('/users/:name/:age', ({ params: { name, age } }, res) =>
+app.put('/users/:name/:age', authMiddleware, ({ params: { name, age } }, res) =>
   res.json({ message: `Your name is ${name} and you are ${age} years old` })
 );
 
