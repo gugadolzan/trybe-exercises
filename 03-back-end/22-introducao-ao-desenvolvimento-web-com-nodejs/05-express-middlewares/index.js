57d3dc5dc8828f26c4e068a5631c486e3086ada9
Gustavo Dolzan <gudolzan@gmail.com>
2022-01-30 20:46:41 -0300

    Create error handler

--- 03-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/05-express-middlewares/index.js
+++ 03-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/05-express-middlewares/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { btcRouter, postsRouter, userRouter } = require('./routes');
+const { HTTP_STATUS_NOT_FOUND } = require('./utils');
 
 const app = express();
 const PORT = 3000;
@@ -13,5 +14,20 @@ app.use('/user', userRouter);
 app.use('/btc', btcRouter);
 
 app.use('/posts', postsRouter);
+
+app.use('*', (_req, _res, next) => {
+  const err = {
+    status: HTTP_STATUS_NOT_FOUND,
+    message: 'Opsss router not found',
+  };
+  next(err);
+});
+
+/**
+ * Error handler
+ */
+app.use((err, _req, res, _next) => {
+  res.status(err.status).json({ message: err.message });
+});
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
