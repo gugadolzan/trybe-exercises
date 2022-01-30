311744f1d0e495875f80b512001f7e3c9d743627
Gustavo Dolzan <gudolzan@gmail.com>
2022-01-30 01:21:03 -0300

    Create GET /ping endpoint

--- 03-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/04-express-http-com-nodejs/index.js
+++ 03-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/04-express-http-com-nodejs/index.js
@@ -6,6 +6,8 @@ app.use(bodyParser.json());
 
 const PORT = 3000;
 
+app.get('/ping', (_req, res) => res.json({ message: 'pong' }));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
