e6a707ad450c7eda88546d70574d346c04c3b374
Gustavo Dolzan <gudolzan@gmail.com>
2022-02-05 16:10:14 -0300

    Implement express-rescue

--- 03-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/01-arquitetura-de-software-camada-de-model/index.js
+++ 03-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/01-arquitetura-de-software-camada-de-model/index.js
@@ -3,39 +3,51 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const Joi = require('joi');
+const rescue = require('express-rescue');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/user', (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
+app.post(
+  '/user',
+  rescue((req, res) => {
+    const { firstName, lastName, email, password } = req.body;
 
-  const { error } = Joi.object({
-    firstName: Joi.string().required().messages({
-      'any.required': `"firstName" is a required field`,
-    }),
-    lastName: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-  })
-    .messages({
-      'string.base': `{#label} should be a type of 'text'`,
-      'string.empty': `{#label} cannot be an empty field`,
-      'string.email': `{#label} should be a valid email address`,
-      'string.min': `{#label} should have a minimum length of {#limit}`,
-      'any.required': `{#label} is a required field`,
+    const { error } = Joi.object({
+      firstName: Joi.string().required().messages({
+        'any.required': `"firstName" is a required field`,
+      }),
+      lastName: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
     })
-    .validate({ firstName, lastName, email, password });
+      .messages({
+        'string.base': `{#label} should be a type of 'text'`,
+        'string.empty': `{#label} cannot be an empty field`,
+        'string.email': `{#label} should be a valid email address`,
+        'string.min': `{#label} should have a minimum length of {#limit}`,
+        'any.required': `{#label} is a required field`,
+      })
+      .validate({ firstName, lastName, email, password });
 
-  if (error)
+    if (error) throw error;
+
+    // create user
+
+    res.status(201).json({ id: 1, firstName, lastName, email });
+  })
+);
+
+app.use((err, _req, res, _next) => {
+  if (err.isJoi)
     return res
       .status(400)
-      .send({ error: true, message: error.details[0].message });
+      .send({ error: true, message: err.details[0].message });
 
-  // create user
+  console.error(err);
 
-  res.status(200).end();
+  res.status(500).send({ error: true, message: 'Internal server error' });
 });
 
 app.listen(process.env.PORT, () => {
