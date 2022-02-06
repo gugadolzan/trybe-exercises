3ae82678ea91a0bcf159f4b159513778510e9dae
Gustavo Dolzan <gudolzan@gmail.com>
2022-02-05 22:25:19 -0300

    Split middlewares

--- 03-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/01-arquitetura-de-software-camada-de-model/middlewares/createUser.js
+++ 03-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/01-arquitetura-de-software-camada-de-model/middlewares/createUser.js
@@ -3,28 +3,35 @@ const rescue = require('express-rescue');
 const UserModel = require('../models/user.model');
 const UserSchema = require('../schemas/user.schema');
 
-module.exports = rescue(async (req, res, next) => {
-  const { firstName, lastName, email, password } = req.body;
+module.exports = [
+  (req, _res, next) => {
+    const { firstName, lastName, email, password } = req.body;
 
-  const { error } = UserSchema.validate({
-    firstName,
-    lastName,
-    email,
-    password,
-  });
+    const { error } = UserSchema.validate({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
 
-  if (error) {
-    const err = new Error(error.details[0].message);
-    err.status = 400;
-    return next(err);
-  }
+    if (error) {
+      const err = new Error(error.details[0].message);
+      err.status = 400;
+      return next(err);
+    }
 
-  const createdUser = await UserModel.create({
-    firstName,
-    lastName,
-    email,
-    password,
-  });
+    next();
+  },
+  rescue(async (req, res, next) => {
+    const { firstName, lastName, email, password } = req.body;
 
-  res.status(201).json(createdUser);
-});
+    const createdUser = await UserModel.create({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+
+    res.status(201).json(createdUser);
+  }),
+];
--- 03-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/01-arquitetura-de-software-camada-de-model/middlewares/updateUser.js
+++ 03-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/01-arquitetura-de-software-camada-de-model/middlewares/updateUser.js
@@ -3,37 +3,48 @@ const rescue = require('express-rescue');
 const UserModel = require('../models/user.model');
 const UserSchema = require('../schemas/user.schema');
 
-module.exports = rescue(async (req, res, next) => {
-  const { firstName, lastName, email, password } = req.body;
-  const { id } = req.params;
+module.exports = [
+  (req, _res, next) => {
+    const { firstName, lastName, email, password } = req.body;
+    const { error } = UserSchema.validate({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
 
-  const { error } = UserSchema.validate({
-    firstName,
-    lastName,
-    email,
-    password,
-  });
+    if (error) {
+      const err = new Error(error.details[0].message);
+      err.status = 400;
+      return next(err);
+    }
 
-  if (error) {
-    const err = new Error(error.details[0].message);
-    err.status = 400;
-    return next(err);
-  }
+    next();
+  },
+  rescue(async (req, _res, next) => {
+    const { id } = req.params;
 
-  const user = await UserModel.findById(id);
+    const user = await UserModel.findById(id);
 
-  if (!user) {
-    const err = new Error('User not found');
-    err.status = 404;
-    return next(err);
-  }
+    if (!user) {
+      const err = new Error('User not found');
+      err.status = 404;
+      return next(err);
+    }
 
-  const updatedUser = await UserModel.update(id, {
-    firstName,
-    lastName,
-    email,
-    password,
-  });
+    next();
+  }),
+  rescue(async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+    const { id } = req.params;
 
-  res.status(200).json(updatedUser);
-});
+    const updatedUser = await UserModel.update(id, {
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+
+    res.status(200).json(updatedUser);
+  }),
+];
