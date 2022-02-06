068ee7ede068b37d800a2e26f7285edf7ea5c3d7
Gustavo Dolzan <gudolzan@gmail.com>
2022-02-06 15:31:05 -0300

    Create formatter functions

--- 03-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/02-arquitetura-de-software-camada-de-controller-e-service/models/cepModel.js
+++ 03-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/02-arquitetura-de-software-camada-de-controller-e-service/models/cepModel.js
@@ -1,6 +1,18 @@
 const connection = require('./connection');
 
+const formatCep = ({ cep, logradouro, bairro, localidade, uf }) => ({
+  cep: cep.replace(/(\d{5})(\d{3})/, '$1-$2'),
+  logradouro,
+  bairro,
+  localidade,
+  uf,
+});
+
+const removeDash = (cep) => cep.replace(/-/g, '');
+
 const createCep = async ({ cep, logradouro, bairro, localidade, uf }) => {
+  cep = removeDash(cep);
+
   const query = `
     INSERT INTO
       ceps
@@ -12,15 +24,14 @@ const createCep = async ({ cep, logradouro, bairro, localidade, uf }) => {
       uf = ?
   `;
 
-  // remove dash
-  cep = cep.replace(/-/g, '');
-
   await connection.execute(query, [cep, logradouro, bairro, localidade, uf]);
 
   return getCep(cep);
 };
 
 const getCep = async (cep) => {
+  cep = removeDash(cep);
+
   const query = `
     SELECT
       cep,
@@ -34,12 +45,9 @@ const getCep = async (cep) => {
       cep = ?
   `;
 
-  // remove dash
-  cep = cep.replace(/-/g, '');
-
   const [rows] = await connection.execute(query, [cep]);
 
-  return rows[0];
+  return rows.length ? formatCep(rows[0]) : null;
 };
 
 module.exports = {
