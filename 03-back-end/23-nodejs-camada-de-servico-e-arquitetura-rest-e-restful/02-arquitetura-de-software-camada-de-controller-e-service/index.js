9f9fd013e857870665a5eba4e32e1e930a34fbfd
Gustavo Dolzan <gudolzan@gmail.com>
2022-02-06 03:10:00 -0300

    Create GET /ping endpoint

--- 03-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/02-arquitetura-de-software-camada-de-controller-e-service/index.js
+++ 03-back-end/23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/02-arquitetura-de-software-camada-de-controller-e-service/index.js
@@ -7,6 +7,8 @@ app = express();
 
 app.use(bodyParser.json());
 
+app.get('/ping', (req, res) => res.status(200).json({ message: 'pong!' }));
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
