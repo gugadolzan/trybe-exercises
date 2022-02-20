4822d38284e198d8c025bb6500f9152a0a43fabc
Gustavo Dolzan <gudolzan@gmail.com>
2022-02-20 18:55:28 -0300

    Create interfaces

---
+++ 03-back-end/26-typescript/02-tipagem-estatica-e-generics/interfaces/index.ts
@@ -0,0 +1,16 @@
+import Pizza from "./Pizza";
+
+const calabresa: Pizza = {
+  flavor: "Calabresa",
+  slices: 8,
+};
+
+const margherita: Pizza = {
+  flavor: "Margherita",
+  slices: 6,
+};
+
+const nutella: Pizza = {
+  flavor: "Nutella",
+  slices: 4,
+};
