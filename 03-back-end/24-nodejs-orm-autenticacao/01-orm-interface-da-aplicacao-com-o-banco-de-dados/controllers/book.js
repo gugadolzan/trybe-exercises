10fa90c5915133111a42be45101a70c7cf21210d
Gustavo Dolzan <gudolzan@gmail.com>
2022-02-20 02:54:42 -0300

    Create CRUD endpoints

---
+++ 03-back-end/24-nodejs-orm-autenticacao/01-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/book.js
@@ -0,0 +1,58 @@
+const { Book } = require('../models');
+
+const findAll = async (_req, res) => {
+  try {
+    const books = await Book.findAll();
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const findById = async (req, res) => {
+  try {
+    const book = await Book.findByPk(req.params.id);
+
+    if (!book) return res.status(404).json({ error: 'Book not found' });
+
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const book = await Book.create(req.body);
+    res.status(201).json(book);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const book = await Book.findByPk(req.params.id);
+    await book.update(req.body);
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    await Book.destroy({ where: { id: req.params.id } });
+    res.status(204).json();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  findAll,
+  findById,
+  create,
+  update,
+  remove,
+};
--- 03-back-end/24-nodejs-orm-autenticacao/01-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
+++ 03-back-end/24-nodejs-orm-autenticacao/01-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const book = require('./controllers/book');
+
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get('/books', book.findAll);
+app.get('/books/:id', book.findById);
+app.post('/books', book.create);
+app.put('/books/:id', book.update);
+app.delete('/books/:id', book.remove);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
