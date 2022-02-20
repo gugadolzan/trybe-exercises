f6572d1b84649d3355ff874e1b38c8c39820e13a
Gustavo Dolzan <gudolzan@gmail.com>
2022-02-20 03:04:57 -0300

    Update endpoints and create GET /author/:name endpoint

--- 03-back-end/24-nodejs-orm-autenticacao/01-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/book.js
+++ 03-back-end/24-nodejs-orm-autenticacao/01-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/book.js
@@ -2,7 +2,12 @@ const { Book } = require('../models');
 
 const findAll = async (_req, res) => {
   try {
-    const books = await Book.findAll();
+    const books = await Book.findAll({
+      order: [
+        ['title', 'ASC'],
+        ['createdAt', 'ASC'],
+      ],
+    });
     res.status(200).json(books);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -33,6 +38,9 @@ const create = async (req, res) => {
 const update = async (req, res) => {
   try {
     const book = await Book.findByPk(req.params.id);
+
+    if (!book) return res.status(404).json({ error: 'Book not found' });
+
     await book.update(req.body);
     res.status(200).json(book);
   } catch (error) {
--- 03-back-end/24-nodejs-orm-autenticacao/01-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
+++ 03-back-end/24-nodejs-orm-autenticacao/01-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const book = require('./controllers/book');
+const author = require('./controllers/author');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(bodyParser.json());
 
 app.get('/books', book.findAll);
 app.get('/books/:id', book.findById);
+app.get('/author/:name', author.getByAuthorName);
 app.post('/books', book.create);
 app.put('/books/:id', book.update);
 app.delete('/books/:id', book.remove);
