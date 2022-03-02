93929e3ac8adde684f3374c64106dd20cf7a3765
Gustavo Dolzan <gudolzan@gmail.com>
2022-03-02 15:21:46 -0300

    Implement admin validation

--- 03-back-end/24-nodejs-orm-autenticacao/03-jwt-json-web-token/hello-jwt/controllers/login.js
+++ 03-back-end/24-nodejs-orm-autenticacao/03-jwt-json-web-token/hello-jwt/controllers/login.js
@@ -13,7 +13,15 @@ module.exports = async (req, res, next) => {
 
   if (error) return next(error);
 
-  const payload = { username, admin: false };
+  if (username === "admin" && password !== "s3nh4S3gur4???") {
+    const err = new Error("Invalid username or password");
+    err.statusCode = 401;
+    return next(err);
+  }
+
+  const admin = username === "admin" && password === "s3nh4S3gur4???";
+
+  const payload = { username, admin };
 
   const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
 
